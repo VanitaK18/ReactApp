d2cbc29dfd742475b547997cfe11cf7b3877000e
VanitaK18 <v.chintaman.khamkar@accenture.com>
2022-05-16 10:36:12 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  let test = "Test";
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {test} React 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
